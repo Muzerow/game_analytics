b01531fd93c60b3fbb62ab2c2bb881e44a93a26c
Kirill Mukhin <70025661+Muzerow@users.noreply.github.com>
2021-02-16 00:46:06 +0300

    Comparison by countries added

--- cohort_ads_finished.R
+++ cohort_ads_finished.R
@@ -1,4 +1,3 @@
-
 library(dplyr)
 library(readr)
 library(stringr)
@@ -14,15 +13,32 @@ parser <- add_option(parser, c("-s", "--fc_start"),
                      help = "First cohort start date")
 parser <- add_option(parser, c("-e", "--fc_end"),
                      help = "First cohort end date")
+parser <- add_option(parser, c("-c", "--fc_country"),
+                     help = "First cohort country")
 parser <- add_option(parser, c("-d", "--sc_start"),
                      help = "Second cohort start date")
 parser <- add_option(parser, c("-r", "--sc_end"),
                      help = "Second cohort end date")
+parser <- add_option(parser, c("-b", "--sc_country"),
+                     help = "Second cohort country")
+parser <- add_option(parser, c("-v", "--interval"),
+                     help = "Event interval")
 args <- parse_args(parser)
+
+# function to subset main cohorts
+
+cohort_subset <- function(data, start, end, country){
+  cohort <- data %>%
+    filter(as.Date(tsEvent) >= start,
+           as.Date(tsEvent) <= end,
+           idCountryISOAlpha2 == country)
+  
+  return(cohort)
+}
 
 # function for aggregating finished ads stats during the first day for each micro-cohort within main cohort
 
-cohort_ads <- function(data, cohort_data, start, end){
+cohort_ads <- function(data, cohort_data, start, end, interval){
   ads_finished_cohort <- data.frame()
   
   start_end_diff <- as.integer(difftime(as.Date(end), as.Date(start), units = "days"))
@@ -31,16 +47,21 @@ cohort_ads <- function(data, cohort_data, start, end){
     coh_data <- cohort_data %>%
       filter(as.Date(tsEvent) == as.Date(i, origin = "1970-01-01"))
     
+    new_data <- data %>%
+      filter(idDevice %in% coh_data$idDevice,
+             eventName == "showAdsFinished",
+             as.Date(tsEvent) %in% seq(as.Date(i, origin = "1970-01-01"), by = "day",
+                                       length.out = as.numeric(interval))) %>%
+      select(idDevice, params.value) %>%
+      count(idDevice, params.value)
+    
+    watchers <- length(unique(new_data$idDevice))
+    
     ads_finished_cohort <- rbind(ads_finished_cohort,
-                                 (data %>%
-                                    filter(idDevice %in% coh_data$idDevice,
-                                           eventName == "showAdsFinished",
-                                           as.Date(tsEvent) %in% seq(as.Date(i, origin = "1970-01-01"), by = "day", length.out = 1)) %>%
-                                    select(idDevice, params.value) %>%
-                                    count(idDevice, params.value) %>%
+                                 (new_data %>%
                                     group_by(params.value) %>%
                                     summarise(cohort_mean = sum(n) / nrow(coh_data),
-                                              cohort_mean_watchers = sum(n) / length(unique(idDevice)))))
+                                              cohort_mean_watchers = sum(n) / watchers)))
   }
   ads_finished_cohort <- ads_finished_cohort %>%
     group_by(params.value) %>%
@@ -48,16 +69,6 @@ cohort_ads <- function(data, cohort_data, start, end){
               cohort_avg_watchers = mean(cohort_mean_watchers))
   
   return(ads_finished_cohort)
-}
-
-# function to subset main cohorts
-
-cohort_subset <- function(data, start, end){
-  cohort <- data %>%
-    filter(as.Date(tsEvent) >= start,
-           as.Date(tsEvent) <= end)
-  
-  return(cohort)
 }
 
 # main function to create a table
@@ -75,27 +86,31 @@ main <- function(args){
     ungroup() %>%
     filter(eventName == "maxOpenBlock",
            params.value == "['1']") %>%
-    select(idDevice, tsEvent)
+    select(idCountryISOAlpha2, idDevice, tsEvent)
   
   first_cohort <- cohort_subset(data = cohorts,
                                 start = args$fc_start,
-                                end = args$fc_end)
+                                end = args$fc_end,
+                                country = args$fc_country)
   
   second_cohort <- cohort_subset(data = cohorts,
                                  start = args$sc_start,
-                                 end = args$sc_end)
+                                 end = args$sc_end,
+                                 country = args$sc_country)
   
   ads_finished_first_cohort <- cohort_ads(data = data,
                                           cohort_data = first_cohort,
                                           start = args$fc_start,
-                                          end = args$fc_end) %>%
+                                          end = args$fc_end,
+                                          interval = args$interval) %>%
     rename(first_cohort_avg = cohort_avg,
            first_cohort_avg_watchers = cohort_avg_watchers)
   
   ads_finished_second_cohort <- cohort_ads(data = data,
                                            cohort_data = second_cohort,
                                            start = args$sc_start,
-                                           end = args$sc_end) %>%
+                                           end = args$sc_end,
+                                           interval = args$interval) %>%
     rename(second_cohort_avg = cohort_avg,
            second_cohort_avg_watchers = cohort_avg_watchers)
   
@@ -105,7 +120,9 @@ main <- function(args){
     mutate(diff = second_cohort_avg - first_cohort_avg,
            diff_watchers = second_cohort_avg_watchers - first_cohort_avg_watchers)
   
-  write.xlsx(cohort_ads_finished, paste(args$outdir, "/", base, ".cohort_ads_finished.xlsx", sep = "", collapse = ""))
+  cohort_ads_finished[,c("params.value", "first_cohort_avg", "second_cohort_avg", "diff",
+                         "first_cohort_avg_watchers", "second_cohort_avg_watchers", "diff_watchers")] %>%
+    write.xlsx(paste(args$outdir, "/", base, ".cohort_ads_finished.xlsx", sep = "", collapse = ""))
 }
 
 main(args)
